f1f1037edc7a33111ce9747ab2553dc2b8e5d07b
Tanmay Raj <tr4236@gmail.com>
2026-08-17 10:04:47 +0530

    improve layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import { AppShell } from '../components/AppShell';
 import type { ReactNode } from 'react';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 
 export const metadata = {
   title: 'GATE PYQ Practice Engine',
@@ -13,7 +15,7 @@ const mathJaxConfigScript = `window.MathJax={tex:{inlineMath:[['$','$'],['\\\\('
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="dark" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
         <script dangerouslySetInnerHTML={{ __html: mathJaxConfigScript }} />
